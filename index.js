7d12ad6021329f2b98a8878016ac1e10da3fe0ed
Joshua Hunt <jelijahhunt@gmail.com>
2020-03-10 23:32:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 object[key]= value
 return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  let newObj= delete object[key]
+return newObj
+}
