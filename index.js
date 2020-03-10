415e3b720097a4eaff9c201bd5967e4ffa36a33a
Joshua Hunt <jelijahhunt@gmail.com>
2020-03-10 23:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-Object.assign({}, Object, {[key]: value})
-return Object.assign{}
+let newObj = Object.assign({}, Object, {[key]: value})
+return newObj
 }
